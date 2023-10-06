3fb79c72dd6ef9f8961ed131a86db199bde2e1c5
Aashish Katila <aashishkatila@gmail.com>
2023-10-06 21:33:52 +0545

    Used  country state city API and completed

--- app.js
+++ app.js
@@ -1,71 +1,56 @@
-var countrySateCityinfo = {
-    Australia: {
-        "Western Australia": {
-            Broome: ["6725", "6318", "6701"],
-            Coolgardie: ["6429", "6432"]
-        },
-        Tasmania: {
-            Hobart: ["7000", "7520"],
-            Launceston: ["7250", "7334"],
-            Burnie: ["7320", "7315"]
-        }
-    },
-    Germany: {
-        Bavaria: {
-            Munich: ["80331", "80333", "80335"],
-            Numemberg: ["90402", "90403", "90404"]
-        },
-        Hessen: {
-            Frankfurt: ["60306", "60309", "60310"],
-            Surat: ["55246", "55247", "55248", "55249"]
-        }
-    },
 
-    Canada: {
-        Alberta: {
-            Calgary: ["8033", "8333", "8035"],
-            Edmonton: ["9040", "9403", "9040"]
-        },
-        Manitoba: {
-            Brandon: ["6030", "6030"],
-            Winnipeg: ["5524", "5547", "5248"]
-        }
-    }
-    
+const config ={
+  apiUrl : 'https://api.countrystatecity.in/v1/countries',
+  apiKey : 'YOUR API KEY'
 }
 
-window.onload = function(){
-    const selectCountry = document.getElementById('country'),
-    selectState = document.getElementById('state')
-    selects = document.querySelectorAll('select')
+
+var selectCountry = document.querySelector('.country'),
+    selectState = document.querySelector('.state')
 
 
-    selectState.disabled = true;
+function loadCountries(){
+  selectState.disabled = true;
 
-    // selectState.forEach(select => {
-    //     if(select.disabled == true){
-    //         select.style.cursor = 'auto'
-    //     }
-    //     else{
-    //         select.style.cursor - 'pointer'
-    //     }
-    // })
-
-    //displays option 
-    for(let country in countrySateCityinfo){
-        selectCountry.options[selectCountry.options.length] = new Option(country,country)
-    }
-
-    selectCountry.onchange = (e) =>{
-        selectState.disabled = false
+  fetch(config.apiUrl,{headers:{"X-CSCAPI-KEY": config.apiKey}})
+  .then(response => response.json())
+  .then(data =>{
+    // console.log(data)
     
-
-    selectState.length = 1
-
-    for(let state in countrySateCityinfo[e.target.value]){
-        // console.log(state)
-        selectState.options[selectState.options.length] = new Option(state,state)
-    }
-    }
-    
+    data.forEach(country => {
+      const option = document.createElement('option')
+      option.value = country.iso2
+      option.textContent = country.name
+      selectCountry.appendChild(option)
+    });
+  })
+  .catch(error => console.log('Error loading countries, ', error))
+ 
 }
+
+
+function loadStates(){
+  selectState.disabled = false;
+
+  const selectedCountryCode = selectCountry.value;
+  // console.log(selectedCountryCode)
+
+  fetch(`${config.apiUrl}/${selectedCountryCode}/states`,{headers:{"X-CSCAPI-KEY": config.apiKey}})
+  .then(response => response.json())
+  .then(data =>{
+    // console.log(data)
+
+    data.forEach(state =>{
+      const option = document.createElement('option')
+      option.value = state.iso2
+      option.textContent = state.name
+      selectState.appendChild(option) 
+    })
+  })
+  .catch(error => console.error('Error loading states: ',error))
+}
+
+
+window.onload = loadCountries;
+
+
